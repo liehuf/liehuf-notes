679745d30870fbe34e46451aef7d010fff3f633d
猎户 <17715076182@163.com>
2025-06-17 21:58:02 +0800

    删除冗杂，更新布局

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,9 +1,8 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'stm32',link: '/one/',},
-  { text: '深度学习',link: '/two/',},
-  { text: 'K230', link: '/three/' },
+  { text: 'K230', link: '/pages/K230/' },
+  { text: 'Rust嵌入式', link: '/pages/Rust/' },
   { text: '留言板', link: '/message-board/' },
   { text: '我的博客', link: 'https://github.com/liehuf/' },
 ]
