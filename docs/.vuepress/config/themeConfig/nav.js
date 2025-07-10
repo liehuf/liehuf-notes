efdeaf8690c572fa5fd0a9639513ed2826cd7361
猎户 <17715076182@163.com>
2025-07-10 22:16:53 +0800

    修复部分错误

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,7 +2,7 @@
 module.exports = [
   { text: '首页', link: '/' },
   { text: 'K230', link: '/pages/K230/' },
-  { text: 'Rust嵌入式', link: '/pages/Rust/' },
+  { text: 'Verilog', link: '/pages/Verilog/' },
   { text: '留言板', link: '/message-board/' },
   { text: '我的博客', link: 'https://github.com/liehuf/' },
 ]
