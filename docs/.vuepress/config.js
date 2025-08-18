14efffeaadf9abc25d11e5f4951373408d5df898
猎户 <17715076182@163.com>
2025-08-18 19:42:17 +0800

    修改导航栏标题名

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "liehuf-notes",
+  title: "原码纪事",
   description: 'vdoing博客主题模板',
   base: '/', // 格式：'/<仓库名>/'， 默认'/'
   //base: '/liehuf-notes/', // 格式：'/<仓库名>/'， 默认'/'
