4020a7981bd1b4227f0b5c11a06ffe5b81561116
猎户 <17715076182@163.com>
2025-08-29 10:20:29 +0800

    修正博客结构

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -3,7 +3,7 @@ module.exports = [
   { text: '首页', link: '/' },
   { text: 'Verilog', link: '/pages/Verilog/' },
   { text: 'Rust', link: '/pages/Rust/' },
-  { text: '深度学习', link: '/pages/Dplearning/' },
+  { text: '算法', link: '/pages/Algo/' },
   { text: '留言板', link: '/message-board/' },
   { text: '我的博客', link: 'https://blog.yuanmajishi.top/' },
 ]
